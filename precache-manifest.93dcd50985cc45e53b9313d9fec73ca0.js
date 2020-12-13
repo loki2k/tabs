48cedb6ed06e4ca8ce9e9848eca9303470eefd43
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:27:44 -0800

    Updates

---
+++ precache-manifest.93dcd50985cc45e53b9313d9fec73ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4144c5d6789f040bb7f348c7b08b7e7a",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "1caa237ddcf37efa3513",
+    "url": "/tabs/static/css/main.77ad5ced.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1caa237ddcf37efa3513",
+    "url": "/tabs/static/js/main.190239a2.chunk.js"
+  },
+  {
+    "revision": "c99201bb389f8c23b139",
+    "url": "/tabs/static/js/runtime-main.e9ac576b.js"
+  }
+]);
